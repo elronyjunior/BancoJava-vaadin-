cfca92ad7740e07ccd323f43e1d5d7b8aaf60be3
RONALDO ARAUJO SILVA JUNIOR <0031432512026@lti.fatecso.edu.br>
2026-03-31 16:28:59 -0300

    initial

---
+++ src/main/java/com/bc/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.bc.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/com/bc/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package com.bc.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
